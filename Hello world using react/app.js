85eb2fb7fc694fb0d80e922c2823ecc2fa29b2b7
Aditya Nidhonkar <adityanidhonkar2004@gmail.com>
2024-01-20 13:08:26 +0530

    added

--- Hello world using react/app.js
+++
@@ -1,8 +0,0 @@
-//Here the heading is object of javascript
-const heading = React.createElement("h1", {}, "Hello World From React");
-console.log(heading);
-
-const root = ReactDOM.createRoot(document.getElementById("root"));
-
-//.render function is to create that h1 tag to display in browser
-root.render(heading);
---
+++ src/utils/constants.js
@@ -0,0 +1,13 @@
+// export const MENU_IMAGES_URL =
+//   "https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/";
+export const LOGO =
+  "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSd2qzMrQs-za4ZGOOBdiUp9Rs7U3Oiya2rugXeoS5M5YgKg5yadl1TisDBIIRDf5AQbiM&usqp=CAU";
+// Image CDN URL for Restaurant card
+export const IMG_CDN_URL =
+  "https://res.cloudinary.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_508,h_320,c_fill/";
+
+// Swiggy API to get Restaurant data with foodfire erver
+export const swiggy_api_URL =
+  "https://foodfire.onrender.com/api/restaurants?lat=21.1702401&lng=72.83106070000001&page_type=DESKTOP_WEB_LISTING";
+
+export const shimmer_card_unit = 20;
